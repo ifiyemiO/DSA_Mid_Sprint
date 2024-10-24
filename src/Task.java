b41d445d58e1b555640e22d3fadfb027d044f210
ifiyemiO <ifiyemi.owi@keyin.com>
2024-10-23 21:58:52 -0230

    user

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private String description;
+    private String isCompleted;
+
+    public Task(String description, String isCompleted) {
+        this.description = description;
+        this.isCompleted = String.valueOf(false);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(String isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public void markAsCompleted(){
+        this.isCompleted = String.valueOf(true);
+    }
+
+    public String isCompleted() {
+        return isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "description='" + description + '\'' +
+                ", isCompleted='" + isCompleted + '\'' +
+                '}';
+    }
 }
