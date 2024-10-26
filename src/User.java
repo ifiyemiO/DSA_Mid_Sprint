de6ca895b1aeea6dec4dedc25eca025bf03ecc78
ifiyemiO <ifiyemi.owi@keyin.com>
2024-10-26 11:58:04 -0230

    user

--- src/User.java
+++ src/User.java
@@ -1,16 +1,36 @@
 public class User {
     private String name;
+    private TaskList taskList;
+
 
     public User(String name) {
         this.name = name;
+        this.taskList = new TaskList();
     }
 
     public String getName() {
         return name;
     }
 
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void addTask(String description) {
+        taskList.addTask(description);
+    }
+
+    public void markTaskAsCompleted(String description) {
+        taskList.markTaskAsCompleted(description);
+    }
+
+    public void printTasks() {
+        System.out.println("Tasks for " + name + ":");
+        taskList.printTasks();
     }
 
     @Override
